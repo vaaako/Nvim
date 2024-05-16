8dba2d1c9a33350832fb7c10e2d7e6ffd1fc8d32
vaaako <matharth520@gmail.com>
2024-05-15 22:01:55 -0300

    new configs

--- lua/user/config/nvimtree.lua
+++ lua/user/config/nvimtree.lua
@@ -3,6 +3,27 @@ if not status_ok then
 	return
 end
 
+
+-- local function on_attach(bufnr)
+-- 	local api = require("nvim-tree.api")
+--
+-- 	local function opts(desc)
+-- 		return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+-- 	end
+--
+--
+-- 	-- local function api_examples()
+-- 	-- 	-- local node = api.tree.get_node_under_cursor()
+-- 	-- 	-- local is_folder = node.fs_stat and node.fs_stat.type == 'directory' or false
+-- 	-- 	-- Just to remember if someday i need it
+-- 	-- 	-- node.nodes ~= nil -> folder -> else -> file
+-- 	-- end
+--
+-- 	vim.keymap.set('n', '?', api.diagnostics.hi_test, opts('Edit'))
+-- end
+
+
+
 nvimtree.setup({
 	sort = {
 		sorter = "case_sensitive"
@@ -10,6 +31,14 @@ nvimtree.setup({
 
 	view = {
 		width = 25,
+		adaptive_size = true,
+		side = "left",
+		-- mappings = {
+		-- 	list = {
+		-- 		-- { key = "<cr>", cb: }
+		-- 		{ key = "v", action = "vsplit" },
+		-- 	}
+		-- }
 	},
 
 	filters = {
@@ -20,8 +49,39 @@ nvimtree.setup({
 		enable = true,
 	},
 
-	-- Change git icons
+
+	-- Show diagnostics
+	diagnostics = {
+		enable = true,
+		show_on_dirs = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+
+	-- Opened file focus
+	update_focused_file = {
+		enable = true,
+		update_cwd = false,
+		ignore_list = {}
+	},
+
 	renderer = {
+
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				item = "│ ",
+				none = "	",
+			}
+		},
+
+		-- Change git icons
 		icons = {
 			glyphs = {
 				git = {
@@ -35,5 +95,10 @@ nvimtree.setup({
 				}
 			}
 		}
-	}
+	},
+
+
+	-- My custom keymaps
+	-- on_attach = on_attach
 })
+
